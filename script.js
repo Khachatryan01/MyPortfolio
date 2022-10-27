270b41dad775f740d0751e334c20b2788444b36e
Khachatryan01 <khachatryanr055@gmail.com>
2022-10-27 14:41:59 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+// let a = 5
+// let b =6
+// if(a>b && a>3){
+// console.log(a**2+b**2);
+// }
+// else{
+//     console.log(a**3+b**3)
+// }
+   
+
+
+
+// let a=5
+// let b=6
+// let c=7
+// if(a>b && c>b){
+//     console.log(a**2+c**2);
+// }else if(b>a && c>a){
+//     console.log(b**2+c**2);
+// }else{
+//     console.log(a**2+b**2);  
+// }
+
+// let a = prompt("")
+// if(a>0){
+//     console.log(+a)
+// }else if (a=0){
+//     console.log(0)
+// }else{
+//     console.log(-a)
+// }
+
+
+
+// let a=prompt("")
+// if(a%4===0 && a%100>0 || a%100<0  ){
+//     console.log("nahanj tari")
+// }else if(a%400===0){
+//     console.log("nahanj tari")
+// }else{
+//     console.log("voch nahanj tari")
+// }
+
+
+
+
+
+
